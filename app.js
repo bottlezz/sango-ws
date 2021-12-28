0eecba6d246771f7a7ef70c7f2ee0317e9a44eb9
Gregory Zhou <bottlezz2002@gmail.com>
2021-12-27 19:35:29 -0800

    setup ws

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || 3000
+import { WebSocketServer } from 'ws';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const wss = new WebSocketServer({ port: 8080 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+
+
