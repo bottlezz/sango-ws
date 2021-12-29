b7d7d418bdf82ce3c0feec10594cb54475b95e98
Gregory Zhou <bottlezz2002@gmail.com>
2021-12-28 23:25:01 -0800

    change back dirname

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+import express from 'express';
+import { WebSocketServer  } from 'ws';
 
-app.use(express.static(__dirname + "/"))
+const PORT = process.env.PORT || 3000;
+const INDEX = 'index.html';
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-var server = http.createServer(app)
-server.listen(port)
+const wss = new WebSocketServer({ server });
 
-console.log("http server listening on %d", port)
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
