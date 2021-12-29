67d6a8fb7dd9d3605131b1e3bf6b8a192943f683
Gregory Zhou <bottlezz2002@gmail.com>
2021-12-28 23:21:47 -0800

    update

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-import express from 'express';
-import { WebSocketServer  } from 'ws';
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-const PORT = process.env.PORT || 3000;
-const INDEX = 'index.html';
-const __dirname='';
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.use(express.static(__dirname + "/"))
 
-const wss = new WebSocketServer({ server });
+var server = http.createServer(app)
+server.listen(port)
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+console.log("http server listening on %d", port)
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
